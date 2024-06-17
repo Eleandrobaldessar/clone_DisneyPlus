1db7998576091839f382de74ee032a826d48deff
Eleandro <baldessareleandro@gmail.com>
2024-06-17 19:14:49 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    });
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
-            
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            const abaAlvo = botao.currentTarget.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
 
             // Retornando a função esconder todas abas
             escondeTodasAbas();
@@ -14,10 +27,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Retornando a função botão
             removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active');
-        })
+            botao.currentTarget.classList.add('shows__tabs__button--is-active');
+        });
     }
-})
+
+    // Seção FAQ, acordeão
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+});
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.currentTarget.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
 // Função para remover botão ativo
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -25,7 +61,6 @@ function removeBotaoAtivo() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
-
 }
 
 // Função para esconder todas as abas
